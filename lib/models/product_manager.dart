66c6c62a0233523d8d83641a2da9b2057f65f12b
AdalbertoMaranhao <31636724+AdalbertoMaranhao@users.noreply.github.com>
2020-07-17 19:28:12 -0300

    implementação do campo de busca por produtos, com autofocus, e alterações do icone e da barra de titulos

--- lib/models/product_manager.dart
+++ lib/models/product_manager.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:lojavirtual/models/product.dart';
 
-class ProductManager extends ChangeNotifier{
+class ProductManager extends ChangeNotifier {
   ProductManager() {
     _loadAllProucts();
   }
@@ -10,6 +10,28 @@ class ProductManager extends ChangeNotifier{
   final Firestore firestore = Firestore.instance;
 
   List<Product> allProducts = [];
+
+  String _search = '';
+
+  String get search => _search;
+
+  set search(String value) {
+    _search = value;
+    notifyListeners();
+  }
+
+  List<Product> get filteredProducts {
+    final List<Product> filteredProducts = [];
+
+    if (search.isEmpty) {
+      filteredProducts.addAll(allProducts);
+    } else {
+      filteredProducts.addAll(allProducts
+          .where((p) => p.name.toLowerCase().contains(search.toLowerCase())));
+    }
+
+    return filteredProducts;
+  }
 
   Future<void> _loadAllProucts() async {
     final QuerySnapshot snapProduct =
--- lib/screens/products/products_screen.dart
+++ lib/screens/products/products_screen.dart
@@ -1,23 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/common/custom_drawer/custom_drawer.dart';
 import 'package:lojavirtual/models/product_manager.dart';
 import 'package:lojavirtual/screens/products/components/products_list_tile.dart';
 import 'package:provider/provider.dart';
+
+import 'components/search_dialog.dart';
 
 class ProductScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: CustomDrawer(),
       appBar: AppBar(
-        title: const Text("Produtos"),
+        title: Consumer<ProductManager>(
+          builder: (_, productManager, __){
+            if(productManager.search.isEmpty){
+              return const Text("Produtos");
+            } else {
+              return LayoutBuilder(
+                builder: (_, contrains){
+                  return GestureDetector(
+                    onTap: () async {
+                      final search = await showDialog<String>(
+                          context: context, builder: (_) => SearchDialog(productManager.search));
+                      if(search != null){
+                        productManager.search = search;
+                      }
+                    },
+                    child: Container(
+                        width: contrains.biggest.width,
+                        child: Text(
+                          productManager.search,
+                          textAlign: TextAlign.center,
+                        ),
+                    ),
+                  );
+                },
+              );
+            }
+          },
+        ),
         centerTitle: true,
+        actions: <Widget>[
+          Consumer<ProductManager>(
+              builder: (_, productManager, __){
+                if(productManager.search.isEmpty){
+                  return IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: () async {
+                      final search = await showDialog<String>(
+                          context: context, builder: (_) => SearchDialog(productManager.search));
+                      if(search != null){
+                        productManager.search = search;
+                      }
+                    },
+                  );
+                } else {
+                  return IconButton(
+                    icon: Icon(Icons.close),
+                    onPressed: () async {
+                      productManager.search = '';
+                    },
+                  );
+                }
+              }
+          ),
+        ],
       ),
       body: Consumer<ProductManager>(
-        builder: (_, productManager, __){
+        builder: (_, productManager, __) {
+          final filteredProducts = productManager.filteredProducts;
           return ListView.builder(
             padding: const EdgeInsets.all(4),
-            itemCount: productManager.allProducts.length,
-            itemBuilder: (_, index){
-              return ProductListTile(productManager.allProducts[index]);
+            itemCount: filteredProducts.length,
+            itemBuilder: (_, index) {
+              return ProductListTile(filteredProducts[index]);
             },
           );
         },
